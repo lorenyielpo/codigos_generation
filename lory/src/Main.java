28dd7ae152a90992548f99afc8453b1738c62843
Loreny Ielpo <43501554+lorenyielpo@users.noreply.github.com>
2019-05-29 08:09:30 -0300

    Java

---
+++ lory/src/Main.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*float n1, n2, media;
+        Scanner dado = new Scanner(System.in);
+
+        System.out.println("Digite a nota1: ");
+        n1 = dado.nextFloat();
+
+        System.out.println("Digite a nota2: ");
+        n2 = dado.nextFloat();
+
+        media = (n1+n2)/2;
+        System.out.println("Média: " + media);*/
+
+
+        /*int n1, n2, n3, n4, soma;
+        Scanner dado = new Scanner(System.in);
+
+        System.out.println("Digite a nota1: ");
+        n1 = dado.nextInt();
+
+        System.out.println("Digite a nota2: ");
+        n2 = dado.nextInt();
+
+        System.out.println("Digite a nota3: ");
+        n3 = dado.nextInt();
+
+        System.out.println("Digite a nota3: ");
+        n4 = dado.nextInt();
+
+        soma = (n1+n2+n3+n4);
+        System.out.println("Soma: " + soma);*/
+
+
+        /*float n1, n2, n3, media;
+        Scanner dado = new Scanner(System.in);
+        System.out.println("Digite a nota1: ");
+
+        n1 = dado.nextInt();
+        System.out.println("Digite a nota2: ");
+
+        n2 = dado.nextInt();
+        System.out.println("Digite a nota3: ");
+
+        n3 = dado.nextInt();
+        System.out.println("Digite a nota3: ");
+
+        media = (n1+n2+n3)/3;
+        System.out.println("Soma: " + media);*/
+
+
+        /*float n1, n2, n3, p1, p2, p3, mediaPond;
+        Scanner dado = new Scanner(System.in);
+
+        System.out.println("Digite a nota1: ");
+        n1 = dado.nextInt();
+        System.out.println("Digite o peso1: ");
+        p1 = dado.nextInt();
+
+        System.out.println("Digite a nota2: ");
+        n2 = dado.nextInt();
+        System.out.println("Digite o peso2: ");
+        p2 = dado.nextInt();
+
+        System.out.println("Digite a nota3: ");
+        n3 = dado.nextInt();
+        System.out.println("Digite o peso3: ");
+        p3 = dado.nextInt();
+
+        mediaPond = ((n1 * p1) + (n2 * p2) + (n3 * p3))/(p1 + p2 + p3);
+        System.out.println("Media: " + mediaPond);*/
+
+
+        /*double salario, novoSalario;
+        Scanner dado = new Scanner(System.in);
+
+        System.out.println("Digite o salario: ");
+        salario = dado.nextDouble();
+        novoSalario = salario * 1.25;
+
+        System.out.println("O novo salário é: " + novoSalario);*/
+
+
+
+    }
+}
---
+++ lory/src/MeuPrimeiroPrograma.java
@@ -0,0 +1,15 @@
+public class MeuPrimeiroPrograma{
+
+    public static void main(String[] args) {
+        System.out.println("Hello, hello world");
+
+        int idade = 23;
+
+        if (idade > 20) {
+            System.out.println("Hey hey");
+        } else {
+            System.out.println("Oi oi");
+        }
+    }
+
+}
